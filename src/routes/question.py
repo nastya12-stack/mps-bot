9369e71b3d500419906667ab37fa340eba320c2a
Agatyny <174094458+Agatyny@users.noreply.github.com>
2024-07-02 11:01:53 +0300

    New commit

--- src/routes/question.py
+++ src/routes/question.py
@@ -7,10 +7,10 @@ router = Router()
 
 @router.message(Command("question"))
 async def command_about(message: Message) -> None:
-    await message.answer("This bot was created by @agatyny and @nastya. If there are some problems write @agatyny")
+    await message.answer("This bot was created by @agatyny and @Anastatyahohlova. If there are some problems write @agatyny")
 
 
 @router.callback_query(F.data == "question")
 async def on_callback(query: CallbackQuery) -> None:
     await query.message.answer(
-        "This bot was created by @agatyny and @nastya. If there are some problems write @agatyny")
+        "This bot was created by @agatyny and @Anastatyahohlova. If there are some problems write @agatyny")
