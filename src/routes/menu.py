f97a48e2946e48b680422826e48dd4eebb19d13b
nastya12-stack <174095233+nastya12-stack@users.noreply.github.com>
2024-07-03 10:38:45 +0300

    dack

--- src/routes/menu.py
+++ src/routes/menu.py
@@ -1,16 +1,27 @@
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters import Command
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 router = Router()
 
 
-@router.message(Command("menu"))
-async def show_menu(message: Message) -> None:
-    markup = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="What is МШП?", callback_data="about")],
+def gen_markup() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Что такое МШП?", callback_data="about")],
         [InlineKeyboardButton(text="What is my name?", callback_data="name")],
         [InlineKeyboardButton(text="Who created you?", callback_data="question")],
     ])
 
+
+@router.message(Command("menu"))
+async def show_menu(message: Message) -> None:
+    markup = gen_markup()
+
     await message.answer("Ask me any of these questions.", reply_markup=markup)
+
+
+@router.callback_query(F.data == "menu")
+async def return_to_menu(query: CallbackQuery) -> None:
+    markup = gen_markup()
+
+    await query.message.edit_text("Ask me any of these questions.", reply_markup=markup)
